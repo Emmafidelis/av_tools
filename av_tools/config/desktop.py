141f4cda54f392d54905107c6255a0316e630be8
Sydney Kibanga <skibanga@aakvatech.com>
2026-02-09 13:00:54 +0300

    feat: add Weigh Bridge module and move weighbridge doctypes

---
+++ av_tools/config/desktop.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "module_name": "Weigh Bridge",
+            "type": "module",
+            "label": _("Weigh Bridge"),
+            "color": "blue",
+            "icon": "octicon octicon-file-directory",
+        }
+    ]
